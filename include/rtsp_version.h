72ae734cc7ceff3cf2065a0d52c4501978204e42
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-11-18 12:55:21 +0600

    Add SmolRTSP_RTSPVersion

---
+++ include/rtsp_version.h
@@ -0,0 +1,11 @@
+#ifndef SMOLRTSP_RTSP_VERSION_H
+#define SMOLRTSP_RTSP_VERSION_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t major;
+    uint8_t minor;
+} SmolRTSP_RTSPVersion;
+
+#endif // SMOLRTSP_RTSP_VERSION_H
