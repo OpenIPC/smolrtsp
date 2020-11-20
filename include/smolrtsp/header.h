a8b4611cf104afeed31020db2ebddb40d21adf09
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-11-20 18:30:29 +0600

    Add all the request and response headers

--- include/smolrtsp/header.h
+++ include/smolrtsp/header.h
@@ -8,6 +8,7 @@ typedef struct {
     char *value;
 } SmolRTSP_Header;
 
+// General-header fields:
 #define SMOLRTSP_HEADER_NAME_ACCEPT_RANGES      "Accept-Ranges"
 #define SMOLRTSP_HEADER_NAME_CACHE_CONTROL      "Cache-Control"
 #define SMOLRTSP_HEADER_NAME_CONNECTION         "Connection"
@@ -29,6 +30,37 @@ typedef struct {
 #define SMOLRTSP_HEADER_NAME_TRANSPORT          "Transport"
 #define SMOLRTSP_HEADER_NAME_USER_AGENT         "User-Agent"
 #define SMOLRTSP_HEADER_NAME_VIA                "Via"
+
+// Request-header fields:
+#define SMOLRTSP_HEADER_NAME_ACCEPT              "Accept"
+#define SMOLRTSP_HEADER_NAME_ACCEPT_CREDENTIALS  "Accept-Credentials"
+#define SMOLRTSP_HEADER_NAME_ACCEPT_ENCODING     "Accept-Encoding"
+#define SMOLRTSP_HEADER_NAME_ACCEPT_LANGUAGE     "Accept-Language"
+#define SMOLRTSP_HEADER_NAME_AUTHORIZATION       "Authorization"
+#define SMOLRTSP_HEADER_NAME_BANDWIDTH           "Bandwidth"
+#define SMOLRTSP_HEADER_NAME_BLOCKSIZE           "Blocksize"
+#define SMOLRTSP_HEADER_NAME_FROM                "From"
+#define SMOLRTSP_HEADER_NAME_IF_MATCH            "If-Match"
+#define SMOLRTSP_HEADER_NAME_IF_MODIFIED_SINCE   "If-Modified-Since"
+#define SMOLRTSP_HEADER_NAME_IF_NONE_MATCH       "If-None-Match"
+#define SMOLRTSP_HEADER_NAME_NOTIFY_REASON       "Notify-Reason"
+#define SMOLRTSP_HEADER_NAME_PROXY_AUTHORIZATION "Proxy-Authorization"
+#define SMOLRTSP_HEADER_NAME_PROXY_REQUIRE       "Proxy-Require"
+#define SMOLRTSP_HEADER_NAME_REFERRER            "Referrer"
+#define SMOLRTSP_HEADER_NAME_REQUEST_STATUS      "Request-Status"
+#define SMOLRTSP_HEADER_NAME_REQUIRE             "Require"
+#define SMOLRTSP_HEADER_NAME_TERMINATE_REASON    "Terminate-Reason"
+
+// Response-header fields:
+#define SMOLRTSP_HEADER_NAME_AUTHENTICATION_INFO    "Authorization-Info"
+#define SMOLRTSP_HEADER_NAME_CONNECTION_CREDENTIALS "Connection-Credentials"
+#define SMOLRTSP_HEADER_NAME_LOCATION               "Location"
+#define SMOLRTSP_HEADER_NAME_M_TAG                  "M-Tag"
+#define SMOLRTSP_HEADER_NAME_PROXY_AUTHENTICATE     "Proxy-Authenticate"
+#define SMOLRTSP_HEADER_NAME_PUBLIC                 "Public"
+#define SMOLRTSP_HEADER_NAME_RETRY_AFTER            "Retry-After"
+#define SMOLRTSP_HEADER_NAME_UNSUPPORTED            "Unsupported"
+#define SMOLRTSP_HEADER_NAME_WWW_AUTHENTICATE       "WWW-Authenticate"
 
 void SmolRTSP_Header_serialize(
     const SmolRTSP_Header header, SmolRTSP_UserWriter user_writer, void *user_cx);
