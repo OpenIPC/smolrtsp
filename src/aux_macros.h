223d85c7ed39352d69a35a26d48aa658070867b5
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-01-03 16:38:02 +0600

    Remove src/aux_macros.h

--- src/aux_macros.h
+++
@@ -1,8 +0,0 @@
-#ifndef SMOLRTSP_AUX_MACROS_H
-#define SMOLRTSP_AUX_MACROS_H
-
-// Constructs a pointer to an unnamed object of a specified type in-place.
-// See https://en.cppreference.com/w/c/language/compound_literal.
-#define OBJ(type, expr) ((type *)(type[]){expr})
-
-#endif // SMOLRTSP_AUX_MACROS_H
