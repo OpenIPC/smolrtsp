e2e4ef6a4cebc524025ed5ce1323a96e83b60eb2
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-01-14 17:59:08 +0600

    libs/slice99 -> tests/slice99

--- scripts/check-fmt.sh
+++ scripts/check-fmt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./run-clang-format/run-clang-format.py --exclude tests/build -r include src tests
+./run-clang-format/run-clang-format.py \
+    --exclude tests/slice99 \
+    --exclude tests/build \
+    -r include src tests
--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find include src tests -iname *.h -o -iname *.c -path tests/build -prune | xargs clang-format -i
+find include src tests -iname *.h -o -iname *.c \
+    -path tests/build -path tests/slice99 -prune \
+    | xargs clang-format -i
