9791f64948d2b9b50f449021371152ea2157e6be
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-04-10 13:32:48 +0600

    Add smolrtsp_file_stream_writer

--- include/smolrtsp/user_writer.h
+++ include/smolrtsp/user_writer.h
@@ -19,7 +19,7 @@
 typedef void (*SmolRTSP_UserWriter)(Slice99 data, void *user_cx);
 
 /**
- * A writer that appends @p data to the array of characters @p buffer.
+ * A writer which appends @p data to the array of characters @p buffer.
  *
  * @param[in] data The slice to the supplied data.
  * @param[out] buffer The character buffer to write to.
@@ -28,5 +28,13 @@ typedef void (*SmolRTSP_UserWriter)(Slice99 data, void *user_cx);
  * `strncat`.
  */
 void smolrtsp_char_buffer_writer(Slice99 data, void *buffer);
+
+/**
+ * A user writer which writes @p data to the file stream @p stream.
+ *
+ * @param[in] data The slice to the supplied data.
+ * @param[out] stream The file stream of type `FILE *` to write to.
+ */
+void smolrtsp_file_stream_writer(Slice99 data, void *stream);
 
 #endif // SMOLRTSP_USER_WRITER_H
