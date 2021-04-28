662d7be819d6ae3a2aef40d3c891cdf123e73c56
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-04-29 00:31:57 +0600

    Temporarily disable scan-build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-cmake --version
-
 mkdir build -p
 cd build
 cmake ..
-# scan-build --use-analyzer /usr/bin/clang cmake -DCMAKE_C_COMPILER=clang --build .
-cmake -DCMAKE_C_COMPILER=clang --build .
 cmake --build .
