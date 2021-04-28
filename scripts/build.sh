8c156ea2700630062bfee6506aed232961b0c4e3
Temirkhan Myrzamadi <hirrolot@gmail.com>
2021-04-29 00:17:40 +0600

    Test CMake (CI)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,6 @@ cmake --version
 mkdir build -p
 cd build
 cmake ..
-scan-build --use-analyzer /usr/bin/clang cmake -DCMAKE_C_COMPILER=clang --build .
+# scan-build --use-analyzer /usr/bin/clang cmake -DCMAKE_C_COMPILER=clang --build .
+cmake -DCMAKE_C_COMPILER=clang --build .
+cmake --build .
