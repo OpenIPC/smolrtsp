e22d30cf46e4836d83892f960321ef7fd372e459
Hirrolot <hirrolot@gmail.com>
2022-02-14 04:09:31 +0600

    Write some docs for `SmolRTSP_RtpTransport`

--- include/smolrtsp/rtp_transport.h
+++ include/smolrtsp/rtp_transport.h
@@ -1,6 +1,8 @@
 /**
  * @file
  * An RTP data transport.
+ *
+ * TODO: more docs.
  */
 
 #pragma once
@@ -14,12 +16,26 @@
 #include <slice99.h>
 #include <smolrtsp.h>
 
+/**
+ * An RTP data transport.
+ */
 typedef struct SmolRTSP_RtpTransport SmolRTSP_RtpTransport;
 
+/**
+ * Creates a new RTP transport from the underlying level-4 protocol @p t.
+ */
 SmolRTSP_RtpTransport *SmolRTSP_RtpTransport_new(SmolRTSP_Transport t, uint8_t rtsp_stream_id);
 
+/**
+ * Sends an RTP packet.
+ */
 void SmolRTSP_RtpTransport_send_packet(
     SmolRTSP_RtpTransport *self, uint64_t timestamp_us, bool marker, uint8_t payload_ty,
     uint32_t clock_rate, U8Slice99 data_header, U8Slice99 data);
 
+/**
+ * Implements #SmolRTSP_Droppable_IFACE for #SmolRTSP_RtpTransport.
+ *
+ * See [Interface99](https://github.com/Hirrolot/interface99) for the macro usage.
+ */
 declImplExtern99(SmolRTSP_Droppable, SmolRTSP_RtpTransport);
