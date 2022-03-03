90a8789984fd31460987d347eaffdc0e10c5ff3e
Hirrolot <hirrolot@gmail.com>
2022-03-03 20:48:56 +0600

    Implement `SMOLRTSP_SDP_DESCRIBE`

--- include/smolrtsp/types/sdp.h
+++ include/smolrtsp/types/sdp.h
@@ -8,6 +8,7 @@
 
 #include <smolrtsp/writer.h>
 
+#include <metalang99.h>
 #include <slice99.h>
 
 #include <smolrtsp/priv/compiler_attrs.h>
@@ -60,6 +61,82 @@ ssize_t SmolRTSP_SdpLine_serialize(SmolRTSP_SdpLine self, SmolRTSP_Writer w)
 ssize_t smolrtsp_sdp_printf(
     SmolRTSP_Writer w, SmolRTSP_SdpType ty, const char *restrict fmt,
     ...) SMOLRTSP_PRIV_MUST_USE SMOLRTSP_PRIV_GCC_ATTR(format(printf, 3, 4));
+
+/**
+ * Writes a sequence of SDP lines to @p w.
+ *
+ * @param[out] ret The variable name of type `ssize_t` with the same semantics
+ * as the return value of #smolrtsp_sdp_printf.
+ * @param[out] w The variable name of type `SmolRTSP_Writer` to be provided with
+ * SDP data.
+ *
+ * The rest of arguments represent a non-empty sequence of comma-separated
+ * tuples `(ty, fmt, ...)`, where
+ *  - `ty` is an SDP line type (such as #SMOLRTSP_SDP_VERSION),
+ *  - `fmt` is a `printf`-like format string,
+ *  - and `...` are the `fmt` parameters (may be omitted).
+ *
+ * Under the hood, #SMOLRTSP_SDP_DESCRIBE just generates a #smolrtsp_sdp_printf
+ * function invocation for each variadic argument.
+ *
+ * ## Examples
+ *
+ * @code
+ * char buffer[256] = {0};
+ * const SmolRTSP_Writer sdp = smolrtsp_string_writer(buffer);
+ * ssize_t ret = 0;
+ *
+ * SMOLRTSP_SDP_DESCRIBE(
+ *     ret, sdp,
+ *     (SMOLRTSP_SDP_VERSION, "0"),
+ *     (SMOLRTSP_SDP_ORIGIN, "SmolRTSP 3855320066 3855320129 IN IP4 0.0.0.0"),
+ *     (SMOLRTSP_SDP_SESSION_NAME, "SmolRTSP test"),
+ *     (SMOLRTSP_SDP_CONNECTION, "IN IP4 %s", SERVER_IP_ADDR),
+ *     (SMOLRTSP_SDP_TIME, "0 0"),
+ *     (SMOLRTSP_SDP_MEDIA, "audio %d RTP/AVP %d", AUDIO_PORT,
+ * AUDIO_RTP_PAYLOAD_TYPE),
+ *     (SMOLRTSP_SDP_ATTR, "control:audio"));
+ *
+ * const char *expected =
+ *     "v=0\r\n"
+ *     "o=SmolRTSP 3855320066 3855320129 IN IP4 0.0.0.0\r\n"
+ *     "s=SmolRTSP test\r\n"
+ *     "c=IN IP4 0.0.0.0\r\n"
+ *     "t=0 0\r\n"
+ *     "m=audio 123 RTP/AVP 456\r\n"
+ *     "a=control:audio\r\n";
+ *
+ * assert((ssize_t)strlen(expected) == ret);
+ * assert(strcmp(expected, buffer) == 0);
+ * @endcode
+ */
+#define SMOLRTSP_SDP_DESCRIBE(ret, w, ...)                                     \
+    SMOLRTSP_PRIV_SDP_DESCRIBE(ret, w, __VA_ARGS__)
+
+#ifndef DOXYGEN_IGNORE
+
+#define SMOLRTSP_PRIV_SDP_DESCRIBE(ret, w, ...)                                \
+    do {                                                                       \
+        ssize_t smolrtsp_priv_sdp_ret = 0;                                     \
+        ML99_EVAL(ML99_variadicsForEach(                                       \
+            ML99_compose(                                                      \
+                ML99_appl(v(SMOLRTSP_PRIV_genSdpPrintf), v(ret, w)),           \
+                v(ML99_untuple)),                                              \
+            v(__VA_ARGS__)))                                                   \
+    } while (0)
+
+#define SMOLRTSP_PRIV_genSdpPrintf_IMPL(ret, w, ty, ...)                       \
+    v({                                                                        \
+        smolrtsp_priv_sdp_ret = smolrtsp_sdp_printf(w, ty, __VA_ARGS__);       \
+        if (smolrtsp_priv_sdp_ret < 0) {                                       \
+            break;                                                             \
+        }                                                                      \
+        ret += smolrtsp_priv_sdp_ret;                                          \
+    })
+
+#define SMOLRTSP_PRIV_genSdpPrintf_ARITY 2
+
+#endif // DOXYGEN_IGNORE
 
 /**
  * Protocol Version (`v=`).
--- tests/types/sdp.c
+++ tests/types/sdp.c
@@ -2,6 +2,10 @@
 
 #include <greatest.h>
 
+#define AUDIO_PORT             123
+#define AUDIO_RTP_PAYLOAD_TYPE 456
+#define SERVER_IP_ADDR         "0.0.0.0"
+
 TEST serialize_sdp_line(void) {
     char buffer[20] = {0};
 
@@ -35,7 +39,40 @@ TEST sdp_printf(void) {
     PASS();
 }
 
+TEST sdp_describe(void) {
+    char buffer[256] = {0};
+    const SmolRTSP_Writer sdp = smolrtsp_string_writer(buffer);
+    ssize_t ret = 0;
+
+    // clang-format off
+    SMOLRTSP_SDP_DESCRIBE(
+        ret, sdp,
+        (SMOLRTSP_SDP_VERSION, "0"),
+        (SMOLRTSP_SDP_ORIGIN, "SmolRTSP 3855320066 3855320129 IN IP4 0.0.0.0"),
+        (SMOLRTSP_SDP_SESSION_NAME, "SmolRTSP test"),
+        (SMOLRTSP_SDP_CONNECTION, "IN IP4 %s", SERVER_IP_ADDR),
+        (SMOLRTSP_SDP_TIME, "0 0"),
+        (SMOLRTSP_SDP_MEDIA, "audio %d RTP/AVP %d", AUDIO_PORT, AUDIO_RTP_PAYLOAD_TYPE),
+        (SMOLRTSP_SDP_ATTR, "control:audio"));
+    // clang-format on
+
+    const char *expected =
+        "v=0\r\n"
+        "o=SmolRTSP 3855320066 3855320129 IN IP4 0.0.0.0\r\n"
+        "s=SmolRTSP test\r\n"
+        "c=IN IP4 0.0.0.0\r\n"
+        "t=0 0\r\n"
+        "m=audio 123 RTP/AVP 456\r\n"
+        "a=control:audio\r\n";
+
+    ASSERT_EQ((ssize_t)strlen(expected), ret);
+    ASSERT_STR_EQ(expected, buffer);
+
+    PASS();
+}
+
 SUITE(types_sdp) {
     RUN_TEST(serialize_sdp_line);
     RUN_TEST(sdp_printf);
+    RUN_TEST(sdp_describe);
 }
