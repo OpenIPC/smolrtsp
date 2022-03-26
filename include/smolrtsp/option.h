435bc9ff69de528d68329b090836313008a89142
Hirrolot <hirrolot@gmail.com>
2022-03-26 23:51:36 +0600

    Add `SMOLRTSP_DEF_OPTION_WITH_NAME`

--- include/smolrtsp/option.h
+++ include/smolrtsp/option.h
@@ -13,3 +13,9 @@
  * See [Datatype99](https://github.com/Hirrolot/datatype99) for the macro usage.
  */
 #define SMOLRTSP_DEF_OPTION(T) datatype99(T##Option, (T##_Some, T), (T##_None))
+
+/**
+ * The same as #SMOLRTSP_DEF_OPTION but allows to choose a custom typename.
+ */
+#define SMOLRTSP_DEF_OPTION_WITH_NAME(name, T)                                 \
+    datatype99(name##Option, (name##_Some, T), (name##_None))
