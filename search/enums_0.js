0ebd997d397501efb864b01988ffff73ce424735
widgetii <widgetii@users.noreply.github.com>
2026-05-24 17:45:00 +0000

    deploy: 7b883c86f26a5e8e0b7202bbc87197e6a293da11

--- search/enums_0.js
+++ search/enums_0.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['smolrtsp_5fcontrolflow_0',['SmolRTSP_ControlFlow',['../controller_8h.html#a0a4c29b584a2ddbb29534ff7b6986ba6',1,'controller.h']]],
-  ['smolrtsp_5fjpegxsinterlace_1',['SmolRTSP_JpegXsInterlace',['../jpegxs_8h.html#aadc712f94b7b4a325d329f2107293551',1,'jpegxs.h']]],
-  ['smolrtsp_5fjpegxspackmode_2',['SmolRTSP_JpegXsPackMode',['../jpegxs_8h.html#a9ed73a34b35d9bb6df6280aa96e2fbdf',1,'jpegxs.h']]],
-  ['smolrtsp_5flowertransport_3',['SmolRTSP_LowerTransport',['../util_8h.html#a1913c4e09e6a1c4acc7a875ec14a59ec',1,'util.h']]],
-  ['smolrtsp_5fparsetype_4',['SmolRTSP_ParseType',['../error_8h.html#ac95dd4e524f39bd1d021e19ba9312644',1,'error.h']]]
+  ['smolrtsp_5finterleavedframestatus_1',['SmolRTSP_InterleavedFrameStatus',['../util_8h.html#a5e406474caf1ddf6762815fa01258a53',1,'util.h']]],
+  ['smolrtsp_5fjpegxsinterlace_2',['SmolRTSP_JpegXsInterlace',['../jpegxs_8h.html#aadc712f94b7b4a325d329f2107293551',1,'jpegxs.h']]],
+  ['smolrtsp_5fjpegxspackmode_3',['SmolRTSP_JpegXsPackMode',['../jpegxs_8h.html#a9ed73a34b35d9bb6df6280aa96e2fbdf',1,'jpegxs.h']]],
+  ['smolrtsp_5flowertransport_4',['SmolRTSP_LowerTransport',['../util_8h.html#a1913c4e09e6a1c4acc7a875ec14a59ec',1,'util.h']]],
+  ['smolrtsp_5fparsetype_5',['SmolRTSP_ParseType',['../error_8h.html#ac95dd4e524f39bd1d021e19ba9312644',1,'error.h']]]
 ];
--- util_8h.js
+++ util_8h.js
@@ -5,6 +5,11 @@ var util_8h =
     [ "SmolRTSP_TransportConfig", "structSmolRTSP__TransportConfig.html", "structSmolRTSP__TransportConfig" ],
     [ "SMOLRTSP_CRLF", "util_8h.html#ac04f5517058459181f72077af54c38cd", null ],
     [ "SMOLRTSP_DEFAULT_PORT", "util_8h.html#a54427d6d5e6df6c7d0c9920ca4343e3a", null ],
+    [ "SmolRTSP_InterleavedFrameStatus", "util_8h.html#a5e406474caf1ddf6762815fa01258a53", [
+      [ "SmolRTSP_InterleavedFrameStatus_Complete", "util_8h.html#a5e406474caf1ddf6762815fa01258a53a418e46e84b88f4cfd271b49b5bec6114", null ],
+      [ "SmolRTSP_InterleavedFrameStatus_Partial", "util_8h.html#a5e406474caf1ddf6762815fa01258a53a6e5e4b907d5927a904901c1750ba9eda", null ],
+      [ "SmolRTSP_InterleavedFrameStatus_NotInterleaved", "util_8h.html#a5e406474caf1ddf6762815fa01258a53a5e3a3e31d79c8a9f4ad88c7894385694", null ]
+    ] ],
     [ "SmolRTSP_LowerTransport", "util_8h.html#a1913c4e09e6a1c4acc7a875ec14a59ec", [
       [ "SmolRTSP_LowerTransport_TCP", "util_8h.html#a1913c4e09e6a1c4acc7a875ec14a59eca3d5e8e0c4a3e76d7388a5932e3d54fa6", null ],
       [ "SmolRTSP_LowerTransport_UDP", "util_8h.html#a1913c4e09e6a1c4acc7a875ec14a59eca4133e9bc8c588469f99b8ed758982e44", null ]
@@ -13,6 +18,7 @@ var util_8h =
     [ "SMOLRTSP_DEF_OPTION", "util_8h.html#a2d8f79b7a21db7aa05b327d920dfa966", null ],
     [ "smolrtsp_interleaved_header", "util_8h.html#a96508da46feee8ed0be450abb3e35d96", null ],
     [ "SmolRTSP_LowerTransport_str", "util_8h.html#a23a10cf498970d153910c1e4222d550c", null ],
+    [ "smolrtsp_parse_interleaved_frame", "util_8h.html#a735992b405e04beb669cdd88ebbae501", null ],
     [ "smolrtsp_parse_interleaved_header", "util_8h.html#ae0a7a7670f0477d14cb44ce8c164f250", null ],
     [ "smolrtsp_parse_transport", "util_8h.html#a50c6302c16dc1d19d5a96aae82e2fd2f", null ]
 ];
